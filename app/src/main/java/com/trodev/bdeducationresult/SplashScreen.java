06d70ec11a9b73e19cf0daaeb9996eaf1558db73
alamin-karno <amin15-1951@diu.edu.bd>
2022-10-13 10:24:33 +0600

    BD EDUCATION RESULT: FIXED SPLASH SCREEN FULL SCREEN MODE.

--- app/src/main/java/com/trodev/bdeducationresult/SplashScreen.java
+++ app/src/main/java/com/trodev/bdeducationresult/SplashScreen.java
@@ -2,15 +2,20 @@ package com.trodev.bdeducationresult;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.WindowManager;
 
+@SuppressLint("CustomSplashScreen")
 public class SplashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_screen);
 
         new Handler().postDelayed(new Runnable() {
