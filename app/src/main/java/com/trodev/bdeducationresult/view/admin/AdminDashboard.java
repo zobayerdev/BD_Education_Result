e7a4fc21995f13b68fb8bb6299d219d3e34621f2
zobayer11 <zobayer.dev@gmail.com>
2022-10-24 18:49:35 +0600

    Admin dashboard & some activity code are upgrade

--- app/src/main/java/com/trodev/bdeducationresult/view/admin/AdminDashboard.java
+++ app/src/main/java/com/trodev/bdeducationresult/view/admin/AdminDashboard.java
@@ -12,5 +12,8 @@ public class AdminDashboard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_dashboard);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Admin Contact");
     }
 }
